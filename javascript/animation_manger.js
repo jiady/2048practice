015d8645aa459607f2d28ec9afe393a6e065a7fa
贾冬雨 <893392749@qq.com>
2016-07-06 13:19:29 +0800

    basic structure

---
+++ javascript/animation_manger.js
@@ -0,0 +1,3 @@
+/**
+ * Created by jiadongyu on 16/7/6.
+ */
---
+++ javascript/game_manager.js
@@ -0,0 +1,10 @@
+/**
+ * Created by jiadongyu on 16/7/6.
+ */
+
+function GameManager(size,InputManager,AnimationManger){
+    this.score=0;
+
+}
+
+
